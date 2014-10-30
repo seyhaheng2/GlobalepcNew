296580abac6e75daf331d16fc1cfb5bc80a1f0f5
seyhaheng2 <seyhaheng2@gmail.com>
2014-10-30 12:53:05 +0700

    done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,15 @@ class HomeController < ApplicationController
   def index
     search = params[:query]
     if search.present?
-      @posts = Post.order("created DESC").text_search(search)
+      @posts = Post.order("created_at DESC").text_search(search)
         .paginate(:page => params[:page], :per_page => 12)
     else
       cate_id = params[:id]
       if cate_id
-        @posts = Post.in_category(cate_id).order("created DESC")
+        @posts = Post.in_category(cate_id).order("created_at DESC")
           .paginate(:page => params[:page], :per_page => 12)
       else
-        @posts = Post.paginate(:page => params[:page], :per_page => 12)
+        @posts = Post.order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
       end
     end
   end
